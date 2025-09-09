31852d1cd4673bd916f131e29d4fd76b20d4538f
unknown <srikanth.pedkar@gmail.com>
2025-09-09 19:55:20 +0530

    updating the program reverseNumber.js

--- reverseNumber.js
+++ reverseNumber.js
@@ -3,8 +3,20 @@
 const prompt = require('prompt-sync')();
 const number = Number(prompt('Enter a number: '));
 
-function reverseNumber(number){
-    let reversed_number = 0;
-    reversed_number = (reversed_number * 10) + (number % 10);
+function reverseNumber(num){
+    if(num < 0 || num > 99999){
+        console.error("The given input is out of range i.e. 0 to 99999");
+        return null;
+    }
+
+    let reversedNumber = 0;
+    let tempNum = num;
+    while(tempNum>0){
+        const digit = tempNum % 10;  // Get the last digit
+        reversedNumber = (reversedNumber * 10) + digit;  // Add the digit to the reversed number
+        tempNum = Math.floor(tempNum / 10); // Remove the last digit from the original number
+    }
+    return reversedNumber;
 }
+
 console.log(reverseNumber(number));
